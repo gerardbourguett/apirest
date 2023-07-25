907810aad9385367226d9bc2e7017ae52145a084
Gerard Bourguett <gerard.bourguett@gmail.com>
2023-07-25 19:30:07 -0400

    third-condition

--- apirest/settings.py
+++ apirest/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 DEBUG = "RENDER" not in os.environ
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["apirest-production-90e7.up.railway.app"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
