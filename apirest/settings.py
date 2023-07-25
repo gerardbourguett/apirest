cc5d9207aa74fbe79633e655068f31cf1015fd0f
Gerard Bourguett <gerard.bourguett@gmail.com>
2023-07-25 19:32:22 -0400

    DATABASES = {     "default": dj_database_url.config(         default="sqlite:///db.sqlite3",         conn_max_age=600,     ) }

--- apirest/settings.py
+++ apirest/settings.py
@@ -89,7 +89,6 @@ WSGI_APPLICATION = "apirest.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
         default="sqlite:///db.sqlite3",
         conn_max_age=600,
     )
